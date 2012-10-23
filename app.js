e45b02567a1e79780ed75b649e194720265858e2
Evan Callicoat <diopter@gmail.com>
2012-10-22 19:56:45 -0500

    Setup router for roush, disable cookies/sessions, tweak formatting

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Globals
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+routes = require('./routes'),
+http = require('http'),
+path = require('path');
 
 // App
 var app = express();
@@ -16,8 +16,8 @@ app.configure(function(){
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.cookieParser('your secret here'));
-    app.use(express.session());
+//    app.use(express.cookieParser('your secret here'));
+//    app.use(express.session());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -32,6 +32,28 @@ app.configure('development', function(){
 
 // Routes
 app.get('/', routes.index);
+app.get('/api/:path', function(req, res) {
+    var options = {
+        'port': '8080',
+        'path': req.params.path + '/'
+    };
+
+    var hreq = http.request(options, function(hres) {
+//        res.setEncoding('utf8');
+        res.statusCode = hres.statusCode;
+        res.setHeader('Content-Type', 'application/json');
+
+        hres.on('data', function(chunk) {
+            res.send(chunk);
+        });
+    }).on('error', function(e) {
+        console.log('Got error: ' + e.message);
+        res.statusCode = 500;
+        res.end();
+    });
+
+    hreq.end();
+});
 
 // Create
 http.createServer(app).listen(app.get('port'), function(){
